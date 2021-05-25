390e4b77a649b57a13d59290d273e6f8b31804ba
Aditya Saravana <aditya.saravana@icloud.com>
2021-05-25 13:05:46 -0700

    Works using input field

--- AIWorkout Collector App/AIWorkout Collector /AIWorkout Collector WatchKit Extension/AIWorkout Collector/ServerManager.swift
+++ AIWorkout Collector App/AIWorkout Collector /AIWorkout Collector WatchKit Extension/AIWorkout Collector/ServerManager.swift
@@ -13,7 +13,8 @@ class ServerManager {
     
     func sendPostRequest(fileName: String, completion: ((_ success: Bool) -> Void)!) {
         let boundary = generateBoundary()
-        
+
+    
         let directory = FileManager.documentsDirectoryURL
         let fileBaseName = (fileName as NSString).lastPathComponent
         
@@ -22,8 +23,7 @@ class ServerManager {
         let fileHandle = try? FileHandle(forReadingFrom: fullFilePath)
         let fullData = fileHandle?.readDataToEndOfFile()
         
-        let dataBody = createDataBody(data: fullData!, boundary: boundary, fileName: fileBaseName)
-        
+
         var request = URLRequest(
             url: URL(string: url)!,
             cachePolicy: .reloadIgnoringLocalCacheData
@@ -33,20 +33,35 @@ class ServerManager {
         request.httpMethod = "POST"
         request.setValue("*/*", forHTTPHeaderField: "Accept")
         request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
-        request.httpBody = dataBody
-        request.setValue("\(dataBody.count)", forHTTPHeaderField: "Content-Length")
+        
+        var httpBody = Data()
+        let dataBody = createDataBody(data: fullData!, boundary: boundary, fileName: fileBaseName)
+        let dataString = String(decoding: fullData!, as: UTF8.self)
+        let formFields = ["version": "1.0", "key": "12344321", "content": dataString]
+        
+        for (key, value) in formFields {
+          httpBody.append(convertFormField(named: key, value: value, using: boundary))
+        }
+        
+        
+        httpBody.append(dataBody)
+        request.httpBody = httpBody
+        request.setValue("\(httpBody.count)", forHTTPHeaderField: "Content-Length")
         //        request.setValue("text/html; charset=utf-8", forHTTPHeaderField: "Content-Type")
         print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
         request.allowsCellularAccess = true
         print(request.allHTTPHeaderFields)
+        
+        print(String(decoding: httpBody, as: UTF8.self))
         print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
         
         
-        let task = urlSession.dataTask(with: request) { data, response, error in
-                // Validate response and call handler
+        let task = urlSession.uploadTask(with: request, from: httpBody ) { data, response, error in
+                // Validate response and call handle
+                
                 print(response ?? "Error printing response")
                 print(error ?? "Error printing error")
-                if error != nil {
+                if (error != nil) {
                     completion(false)
                 } else {
                     completion(true)
@@ -60,6 +75,17 @@ class ServerManager {
         return "Boundary-\(NSUUID().uuidString)"
     }
     
+    func convertFormField(named name: String, value: String, using boundary: String) -> String {
+      var fieldString = "--\(boundary)\r\n"
+      fieldString += "Content-Disposition: form-data; name=\"\(name)\"\r\n"
+      fieldString += "\r\n"
+      fieldString += "\(value)\r\n"
+
+      return fieldString
+    }
+
+    
+    
     func createDataBody(data: Data, boundary: String, fileName: String) -> Data {
         
         let lineBreak = "\r\n"
@@ -67,12 +93,12 @@ class ServerManager {
         
         body.append("--\(boundary + lineBreak)")
         body.append("Content-Disposition: form-data; name=\"file\"; filename=\"\(fileName)\"\(lineBreak)")
-        body.append("Content-Type: application/octet-stream\(lineBreak) \(lineBreak)")
+        body.append("Content-Type: text/html; charset=utf-8\(lineBreak) \(lineBreak)")
+        //body.append("Content-Length: \(body.count)")
         body.append(data)
         body.append(lineBreak)
         
         body.append("--\(boundary)--\(lineBreak)")
-        print(String(decoding: body, as: UTF8.self))
         return body
     }
 }
